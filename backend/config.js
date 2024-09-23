c4dd84e2d0ae225cb370faf143dcd291324843c9
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-09-23 16:20:07 +0530

    My first mern project

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+const port = 5555;
+const mongoDBURL =
+  "mongodb+srv://jainmokshejain2005:12345678@mokshe@cluster0.thl2kcu.mongodb.net/";
+
+const obj = {port, mongoDBURL};
+export default obj;
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+const mongoDBURL = "mongodb+srv://vansh:vansh123@cluster0.po6d5co.mongodb.net/";
+const port =5555;
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome To Book Store");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(port, () => {
+      console.log(`App is listening to port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
